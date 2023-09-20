986c63ecc3bfcc680547a305699639302cda329c
Alejandro Villalobos <tamalitodepuerco0@gmail.com>
2023-09-19 23:33:29 -0700

    modificacion login
    se esta modificando el login

--- TAQUERIA_EL_PRIMO/src/login/login.jsx
+++ TAQUERIA_EL_PRIMO/src/login/login.jsx
@@ -1,6 +1,6 @@
 import taco from "../components/assets/taco.png";
 import quesadilla from "../components/assets/quesadilla.png";
-import tortilla from "../components/assets/tortilla.png"
+import tortilla from "../components/assets/tortilla.png";
 import { useState } from "react";
 
 function Login() {
@@ -11,46 +11,30 @@ function Login() {
   }
 
   return (
-    <div className="bg-gray-200 max-w-md mx-auto rounded-md shadow-xl">
-      <div className="grid justify-items-center">
-        <img src={taco} alt="" className="mt-4" />
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden bg-[#FAF0E6]">
+      <div className="w-7/12 p-14 m-28 bg-[#E6E6E6] rounded-3xl shadow-2xl lg:max-w-md">
+        <h1 className="text-2xl font-semibold text-center ">Bienvenido</h1>
+        <form action="" className="mt-8 w-full">
+          <div className="mb-2">
+            <input type="email" className="block w-full px-4 py-4 mt-2  bg-white border rounded-2xl"/>
+          </div>
+          <div className="mb-2">
+            <input type="email" className="block w-full px-4 py-4 mt-8  bg-white border rounded-2xl"/>
+          </div>
+        </form>
       </div>
-      <form className="p-8 mb-1 grid">
-
-        <div className=" w-full rounded-md relative">
-        <input
-          type="text"
-          placeholder="Usuario"
-          autoFocus
-          className="w-full px-2 py-4 text-base border rounded outline-none focus:ring-blue-500 focus:border-blue-500 focus:ring-1 mb-4"
-        />
-        </div>
-
-
-        <div className=" w-full mb-4 rounded-md relative">
-          <input
-            type={contrasenaVisible ? "text" : "password"}
-            placeholder="Contraseña"
-            className="w-full px-2 py-4 text-base border rounded outline-none focus:ring-blue-500 focus:border-blue-500 focus:ring-1"
-          />
-
-          <button type="button"
-            className="absolute inset-y-0 right-0 flex items-center px-4 text-gray-600"
-            onClick={verContrasena}
-          >
-            {contrasenaVisible ? (
-              <img src={tortilla} alt="Tortilla" height="20px" width="32px"/>
-            ) : (
-              <img src={quesadilla} alt="Quesadilla" height="20px" width="32px"/>
-            )}
-          </button>
-        </div>
-        <button className="bg-blue-500 rounded-md text-white px-3 py-1">
-          Iniciar Sesion
-        </button>
-      </form>
     </div>
   );
 }
 
+//<button type="button"
+//className="absolute inset-y-0 right-0 flex items-center px-4 text-gray-600"
+//onClick={verContrasena}
+//>
+//{contrasenaVisible ? (
+//  <img src={tortilla} alt="Tortilla" height="20px" width="32px"/>
+//) : (
+//  <img src={quesadilla} alt="Quesadilla" height="20px" width="32px"/>
+//)}
+//</button>
 export default Login;
