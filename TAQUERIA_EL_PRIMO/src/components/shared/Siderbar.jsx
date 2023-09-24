58078d9774c8b9a1076b5c5f91d8fd3d003e449a
Alejandro Villalobos <tamalitodepuerco0@gmail.com>
2023-09-23 21:21:23 -0700

    sidebar actualizado
    se mejoro el sidebar (sigue en beta)

--- TAQUERIA_EL_PRIMO/src/components/shared/Siderbar.jsx
+++ TAQUERIA_EL_PRIMO/src/components/shared/Siderbar.jsx
@@ -1,43 +1,52 @@
 import React from "react";
-import pencilImage from "../assets/pencil.png";
-import boxImage from "../assets/box.png";
-import { LiaNotesMedicalSolid } from "react-icons/lia";
+import { SlNotebook } from "react-icons/sl";
 import { MdOutlineInventory2 } from "react-icons/md";
 import { BiLogOut } from "react-icons/bi";
+import { BsBoxSeam } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#666f79] fixed left-4 top-24 w-25 h-3/4 rounded-3xl">
-      <h1 className="text-2xl text-black uppercase font-bold text-center my-5"></h1>
+    <div className="bg-red-800 fixed left-4 top-1/2 transform -translate-y-1/2 w-25 h-1/2 rounded-3xl flex flex-col justify-center items-center">
 
-      <ul className="pl-3 pt-28 space-y-5">
-        <li className="bg-[#F8F8F8] p-3 rounded-tl-lg rounded-bl-lg">
-          <a href="#" className="bg-[#DFDFDF] p-2 block rounded-lg">
-            <LiaNotesMedicalSolid />
+      <ul className="pl-3 space-y-5">
+        <li className="p-3 rounded-tl-lg rounded-bl-lg hover:bg-[#F8F8F8] group transition-colors">
+          <a href="#" className="p-2 block rounded-lg group-hover:bg-gray-400 transition-colors">
+            <div className="flex items-center justify-center">
+              <SlNotebook style={{ fontSize: "24px", color: "white" }} />
+            </div>
+          </a>
+        </li>
+
+        <li className="p-3 rounded-tl-lg rounded-bl-lg hover:bg-[#F8F8F8] group transition-colors">
+          <a href="#" className="p-2 block rounded-lg group-hover:bg-gray-400 transition-colors">
+            <div className="flex items-center justify-center">
+              <BsBoxSeam style={{ fontSize: "24px", color: "white" }} />
+            </div>
           </a>
         </li>
 
         <li className="p-3 rounded-tl-lg rounded-bl-lg hover:bg-[#F8F8F8] group transition-colors">
           <a
             href="#"
-            className="p-2 block rounded-lg group-hover:bg-[#DFDFDF] transition-colors"
+            className="p-2 block rounded-lg group-hover:bg-gray-400 transition-colors"
           >
-            <MdOutlineInventory2 />
+            <div className="flex items-center justify-center">
+              <MdOutlineInventory2 style={{ fontSize: "24px", color: "white" }} />
+            </div>
           </a>
         </li>
-      </ul>
 
-      <ul className="pl-3 pt-56 space-y-2">
         <li className="p-3 rounded-tl-lg rounded-bl-lg hover:bg-[#F8F8F8] group transition-colors">
           <a
             href="#"
-            className="p-2 block rounded-lg group-hover:bg-[#DFDFDF] transition-colors"
+            className="p-2 block rounded-lg group-hover:bg-gray-400 transition-colors"
           >
-            <BiLogOut />
+            <div className="flex items-center justify-center">
+              <BiLogOut style={{ fontSize: "24px", color: "white" }} />
+            </div>
           </a>
         </li>
       </ul>
-      
     </div>
   );
 };
